f675cc8b7e05151a66ffd0a3c3e5b70a780ec764
Matt Steele <matt@matt-steele.com>
2015-11-08 21:51:09 -0700

    finish video 12

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,19 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
+var Router  = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
+
 /*
- App
+  App
 */
 
 var App = React.createClass({
-  getInitialState : function(){
+  getInitialState : function() {
     return {
       fishes : {},
       order : {}
@@ -26,20 +27,46 @@ var App = React.createClass({
     // set the state
     this.setState({ fishes : this.state.fishes });
   },
-  loadSamples : function () {
+  loadSamples : function() {
     this.setState({
       fishes : require('./sample-fishes')
     });
   },
+  renderFish : function(key){
+    return <Fish key={key} index={key} details={this.state.fishes[key]} />
+  },
   render : function() {
     return (
-    <div className="catch-of-the-day">
-      <div className="menu">
-        <Header tagline="Fresh Seafood Market"/>
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {Object.keys(this.state.fishes).map(this.renderFish)}
+          </ul>
+        </div>
+        <Order/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
-      <Order/>
-      <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
-    </div>
+    )
+  }
+});
+
+/*
+  Fish
+  <Fish />
+*/
+var Fish = React.createClass({
+  render : function() {
+    var details = this.props.details;
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{h.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+      </li>
     )
   }
 });
@@ -47,13 +74,12 @@ var App = React.createClass({
 
 /*
   Add Fish Form
-  <addFishForm/>
+  <AddFishForm />
 */
 
 var AddFishForm = React.createClass({
-  createFish : function(event){
-
-    // 1. Stop the app from submitting
+  createFish : function(event) {
+    // 1. Stop the form from submitting
     event.preventDefault();
     // 2. Take the data from the form and create an object
     var fish = {
@@ -64,7 +90,6 @@ var AddFishForm = React.createClass({
       image : this.refs.image.value
     }
 
-    console.log(fish);
     // 3. Add the fish to the App State
     this.props.addFish(fish);
     this.refs.fishForm.reset();
@@ -84,24 +109,22 @@ var AddFishForm = React.createClass({
       </form>
     )
   }
-})
-
-
+});
 
 /*
- Header
- <Header/>
+  Header
+  <Header/>
 */
 var Header = React.createClass({
   render : function() {
     return (
       <header className="top">
         <h1>Catch
-        <span className="ofThe">
-          <span className="of">of</span>
-          <span className="the">the</span>
-        </span>
-        Day</h1>
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day</h1>
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
@@ -109,8 +132,8 @@ var Header = React.createClass({
 })
 
 /*
- Order
- <Order/>
+  Order
+  <Order/>
 */
 var Order = React.createClass({
   render : function() {
@@ -121,15 +144,16 @@ var Order = React.createClass({
 })
 
 /*
- Inventory
- <Inventory/>
+  Inventory
+  <Inventory/>
 */
 var Inventory = React.createClass({
   render : function() {
     return (
       <div>
         <h2>Inventory</h2>
-        <AddFishForm {...this.props}/>
+
+        <AddFishForm {...this.props} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
@@ -138,12 +162,12 @@ var Inventory = React.createClass({
 
 
 /*
- StorePicker
- <StorePicker/>
+  StorePicker
+  This will let us make <StorePicker/>
 */
 
 var StorePicker = React.createClass({
-    mixins : [History],
+  mixins : [History],
   goToStore : function(event) {
     event.preventDefault();
     // get the data from the input
@@ -153,7 +177,7 @@ var StorePicker = React.createClass({
   render : function() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
-        <h2>Please Enter A Store {name}</h2>
+        <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit" />
       </form>
@@ -173,6 +197,10 @@ var NotFound = React.createClass({
 });
 
 
+/*
+  Routes
+*/
+
 var routes = (
   <Router history={createBrowserHistory()}>
     <Route path="/" component={StorePicker}/>
@@ -180,11 +208,5 @@ var routes = (
     <Route path="*" component={NotFound}/>
   </Router>
 )
-
-/*
- Routes
-*/
-
-
 
 ReactDOM.render(routes, document.querySelector('#main'));
